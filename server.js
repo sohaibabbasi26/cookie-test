2899fdd6d52a21165acfb408755b1be7fcd648d6
eshatahir1 <eshatahir2111@gmail.com>
2025-05-02 03:24:47 +0500

    config file

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const adminRoutes = require("./src/routes/adminRoutes");
 const userRoutes = require("./src/routes/userRoutes");
 const { syncModels } = require("./src/helpers/modelsSyncing");
 const cron = require("node-cron");
-const { checkAndSendReminders } = require('./src/helpers/pushNotification');
+// const { checkAndSendReminders } = require('./src/helpers/pushNotification');
 const { getTimeZone } = require('./src/helpers/timezoneHelpers');
 const cookieParser = require("cookie-parser");
 
@@ -40,11 +40,11 @@ app.get("/api/test", (request, response) => {
 })
 
 
-cron.schedule('* * * * *', () => {
-  checkAndSendReminders();
-}, {
-  timezone: getTimeZone()
-});
+// cron.schedule('* * * * *', () => {
+//   checkAndSendReminders();
+// }, {
+//   timezone: getTimeZone()
+// });
 
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Route not found' }); 
